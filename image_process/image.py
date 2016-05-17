99a4f8a754ac50a136dc689c1a732d28bd7cbd61
khiemdoancrazy <doankhiem.crazy@gmail.com>
2016-05-18 00:19:48 +0700

    add calc distance function

--- image_process/image.py
+++ image_process/image.py
@@ -1,5 +1,6 @@
 import cv2
 
+
 __author__ = "Anh Do Nguyet"
 
 
@@ -10,13 +11,43 @@ class Image:
         self.__blue_hist = None
         self.__green_hist = None
         self.__red_hist = None
+        self.__category = '0'
 
+    # đọc dữ liệu ảnh đầu vào, tính histogram, chuẩn hoá
     def read(self, file_path):
         self.__file_path = file_path
         img = cv2.imread(file_path, cv2.IMREAD_COLOR)
+
+        # lấy histogram của ảnh
         self.__blue_hist = cv2.calcHist([img], [0], None, [256], [0, 256])
         self.__green_hist = cv2.calcHist([img], [1], None, [256], [0, 256])
         self.__red_hist = cv2.calcHist([img], [2], None, [256], [0, 256])
 
+        # chuẩn hoá histogram
+        cv2.normalize(self.__blue_hist, self.__blue_hist, 0, 255, cv2.NORM_MINMAX)
+        cv2.normalize(self.__green_hist, self.__green_hist, 0, 255, cv2.NORM_MINMAX)
+        cv2.normalize(self.__red_hist, self.__red_hist, 0, 255, cv2.NORM_MINMAX)
+
     def get_file_path(self):
         return self.__file_path
+
+    def get_category(self):
+        return self.__category
+
+    def set_category(self, cat):
+        self.__category = cat
+
+    def get_blue_histogram(self):
+        return self.__blue_hist
+
+    def get_green_histogram(self):
+        return self.__green_hist
+
+    def get_red_histogram(self):
+        return self.__red_hist
+
+    def calc_distance(self, image, method=cv2.HISTCMP_CHISQR):
+        d = cv2.compareHist(self.__blue_hist, image.get_blue_histogram(), method)
+        d += cv2.compareHist(self.__green_hist, image.get_green_histogram(), method)
+        d += cv2.compareHist(self.__red_hist, image.get_red_histogram(), method)
+        return d
--- image_process/manager.py
+++ image_process/manager.py
@@ -1,6 +1,7 @@
 
 import os
 import imghdr
+import cv2
 from .image import Image
 from .database import Database
 
@@ -10,6 +11,7 @@ class Manager:
     def __init__(self):
         self.__database = Database()
         self.__images = []
+        self.__distances = []
 
     def load_image_folder(self, folder_path):
         self.__images = []
@@ -17,9 +19,13 @@ class Manager:
             file_path = os.path.join(folder_path, item)
             if imghdr.what(file_path) is not None:          # xác định xem file đầu vào có phải là file ảnh không
                 image = Image()
+                if len(item) < 7:
+                    image.set_category('0')
+                else:
+                    image.set_category(item[0])
                 image.read(file_path)
                 self.__images.append(image)
-        return True
+                print('Cat: ', image.get_category(), ' - ', image.get_file_path())
 
     def set_database(self, path):
         self.__database.set_path(path)
@@ -33,9 +39,24 @@ class Manager:
         self.__images = self.__database.loads()
 
     def query_image(self, file_path):
-        image = Image()
-        image.read(file_path)
-        print('query image: ', file_path)
+        if imghdr.what(file_path) is not None:          # xác định xem file đầu vào có phải là file ảnh không
+            image = Image()
+            image.read(file_path)
+            self.__distances = []
+            for img in self.__images:
+                d = image.calc_distance(img, cv2.HISTCMP_CHISQR)
+                self.__distances.append(d)
+
+            # sắp xếp kết quả
+            # cùng lúc sắp xếp 2 list __distances và images theo __distances
+            combined = zip(self.__distances, self.__images)
+            z = sorted(combined)
+            self.__distances, self.__images = zip(*z)
+
+            i = 0
+            for d in self.__distances:
+                print('khoang cach ', i, ': ', d, " - ", self.__images[i].get_file_path())
+                i += 1
 
     def get_image(self):
         print('get image: ')
